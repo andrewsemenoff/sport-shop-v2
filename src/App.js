5f57b99c9e5ebc08d7551ab607d463281e5fd502
andrewsemenoff <107771959+andrewsemenoff@users.noreply.github.com>
2023-05-01 10:46:25 +0300

    added categories collection to firebase

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Routes, Route } from 'react-router-dom';
 import {
   onAuthStateChangedListener,
   createUserDocumentFromAuth,
+  addCollectionAndDocuments,
 } from './utils/firebase/firebase.utils';
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
@@ -13,9 +14,14 @@ import Authentication from './routes/authentication/authentication.component';
 import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component';
 import { setCurrentUser } from './store/user/user.reducer';
+import SHOP_DATA from './shop-data';
 
 const App = () => {
   const dispatch = useDispatch();
+  useEffect(() => {
+    addCollectionAndDocuments('categories', SHOP_DATA)
+  }, [])
+  
  
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener((user) => {
